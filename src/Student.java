c4cfebdf73f79a102ea17b3dfcb09423056e4487
aimautb <abolathan3@gmail.com>
2025-04-14 13:32:28 +0500

    test of myarray and mylinked list from moodle and i guess/hope it's final commit :)

---
+++ src/Student.java
@@ -0,0 +1,25 @@
+public class Student implements Comparable<Student> {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + ")";
+    }
+
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+}
