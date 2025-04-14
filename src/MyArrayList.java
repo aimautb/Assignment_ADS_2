c130a2fb1dd98f072904fd719ebe161672a84764
aimautb <abolathan3@gmail.com>
2025-04-14 11:45:56 +0500

    Creating MyArrayList and implemenitng MyLIst to it

---
+++ src/MyArrayList.java
@@ -0,0 +1,155 @@
+import java.util.Iterator;
+
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
+    private Object[] data;
+    private int size;
+
+    public MyArrayList() {
+        data = new Object[10]; // sdes typo nachalka of capacitygoi pochemuto problema wyga bered wyga bered omiiiiririiri reia ifi dis ia
+        size = 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            Object[] newData = new Object[data.length * 2];
+            System.arraycopy(data, 0, newData, 0, size);
+            data = newData;
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        ensureCapacity();
+        data[size++] = item;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        ensureCapacity();
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = item;
+        size++;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        data[index] = item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return (T) data[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        System.arraycopy(data, index + 1, data, index, size - index - 1);
+        size--;
+        data[size] = null;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                T left = (T) data[j];
+                T right = (T) data[j + 1];
+                if (left.compareTo(right) > 0) {
+                    Object temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        System.arraycopy(data, 0, result, 0, size);
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        data = new Object[10];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                return (T) data[index++];
+            }
+        };
+    }
+}
